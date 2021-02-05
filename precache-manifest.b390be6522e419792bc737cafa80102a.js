b353463e696bd5e75c48138c0f4b423335bc0c27
Lucas <lucaspierandrei19@gmail.com>
2021-02-05 11:57:36 -0300

    Updates

---
+++ precache-manifest.b390be6522e419792bc737cafa80102a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd7daa9bfaeaab2c6be4617d69851c5",
+    "url": "/budget%E2%80%8B/index.html"
+  },
+  {
+    "revision": "bacfa9c784d6df07dec5",
+    "url": "/budget%E2%80%8B/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/budget%E2%80%8B/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget%E2%80%8B/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bacfa9c784d6df07dec5",
+    "url": "/budget%E2%80%8B/static/js/main.183b360d.chunk.js"
+  },
+  {
+    "revision": "64e5d298d0d6f5cdb294",
+    "url": "/budget%E2%80%8B/static/js/runtime-main.e6a37d89.js"
+  }
+]);
